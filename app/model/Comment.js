f7b0a8f6e31eae5a66854fd9c4de5f83eed31f9c
Nischay Venkatram <nischay.v@gmail.com>
2016-06-27 21:39:35 -0500

    Added mongo schemas

--- app/model/Comment.js
+++ app/model/Comment.js
@@ -1,3 +1,10 @@
-/**
- * Created by Nischay on 6/25/2016.
- */
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+    body: String,
+    author: String,
+    upvotes: {type: Number, default: 0},
+    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+});
+
+mongoose.model('Comment', CommentSchema);
