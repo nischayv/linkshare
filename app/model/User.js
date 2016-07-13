bced165a2ed1cb143adf613845552d5b47678807
Nischay Venkatram <nischay.v@gmail.com>
2016-07-13 00:01:36 -0500

    created methods for storage and validation of passwords

--- app/model/User.js
+++ app/model/User.js
@@ -3,8 +3,20 @@ var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, unique: true},
-    password: String,
+    hash: String,
     salt: String
 });
+
+UserSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+    return this.hash === hash;
+};
 
 mongoose.model('User', UserSchema);
