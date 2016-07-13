0db8642ad16ae103e46b82fb883eac63585384ba
Nischay Venkatram <nischay.v@gmail.com>
2016-07-13 00:15:22 -0500

    added function for jwt

--- app/model/User.js
+++ app/model/User.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, unique: true},
@@ -18,5 +19,19 @@ UserSchema.methods.validPassword = function(password) {
 
     return this.hash === hash;
 };
+UserSchema.methods.generateJWT = function() {
+
+    // set expiration to 60 days
+    var today = new Date();
+    var exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000),
+    }, 'SECRET');
+};
+
 
 mongoose.model('User', UserSchema);
