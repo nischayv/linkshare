bc6b323ba2657e86317a84ee2514c45f7ebda213
Nischay Venkatram <nischay.v@gmail.com>
2016-07-18 20:53:59 -0500

    Added new rest routes for login

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,8 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 var path = require('path');
+var passport = require('passport');
+var User = mongoose.model('User');
 
 //middleware to find post with id
 router.param('post', function(req, res, next, id) {
@@ -128,6 +130,22 @@ router.post('/register', function(req, res, next){
         }
         return res.json({token: user.generateJWT()})
     });
+});
+
+router.post('/login', function(req, res, next){
+    if(!req.body.username || !req.body.password){
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    passport.authenticate('local', function(err, user, info){
+        if(err){ return next(err); }
+
+        if(user){
+            return res.json({token: user.generateJWT()});
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
 });
 
 module.exports = router;
